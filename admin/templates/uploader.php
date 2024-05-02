1a4fd1d6d81e72aea399f53a87e5d861f0897a73
Plugin Manager <plugin-manager@stefanwarnat.de>
2024-05-02 12:39:16 +0000

    v1.1.0

--- admin/templates/uploader.php
+++ admin/templates/uploader.php
@@ -1,27 +1,14 @@
 <div style="background-color:#fff;border:2px solid #ccc;padding:10px;margin-top:20px;">
     <form method="POST" enctype="multipart/form-data" action="#">
-        <h1><?php echo __('Add Newspaper', 'sw-newspaper'); ?></h1>
-        <table class="form-table" role="presentation">
-            <tbody>
-                <tr class="user-rich-editing-wrap">
-                    <th scope="row"><label for="title">Bezeichnung:</label></th>
-                    <td>
-                        <input type="text" style="width:300px;" name="title" id="title" class="form-control" value="" required />
-                    </td>
-                </tr>
-                <tr class="user-rich-editing-wrap">
-                    <th scope="row"><label for="date">Datum:</label></th>
-                    <td>
-                        <input type="date" name="date" class="form-control" id="date" required />
-                    </td>
-                </tr>
-                <tr class="user-rich-editing-wrap">
-                    <th scope="row"><label for="uploadfile">Neues PDF hochladen:</label></th>
-                    <td>
-                        <input type="file" name="uploadfile" id="uploadfile" class="form-control" value="" required />
-                    </td>
-                </tr>
-        </table>
+        <label>Bezeichnung: <input type="text" name="title" value="" required /></label>
+        <br/>
+        <br/>
+        <label>Datum: <input type="date" name="date" required /></label>
+        <br/>
+        <br/>
+        <label>Neues PDF hochladen: <input type="file" name="uploadfile" value="" required /></label>
+        <br/>
+        <br/>
         <input type="submit" name="submit" class="button button-primary" value="Hochladen" />
     </form>
 </div>
